84ed6294708080985682029a8ecc4b6b8738a7db
benjdevries <55499121+benjdevries@users.noreply.github.com>
2020-12-09 20:28:05 -0500

    work on ui skeleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,40 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NewListComponent } from './new-list/new-list.component';
+import { EditListComponent } from './edit-list/edit-list.component';
+import { ViewListComponent } from './view-list/view-list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { ListCardComponent } from './list-card/list-card.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { ListItemComponent } from './list-item/list-item.component';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NewListComponent,
+    EditListComponent,
+    ViewListComponent,
+    ListCardComponent,
+    ListItemComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
